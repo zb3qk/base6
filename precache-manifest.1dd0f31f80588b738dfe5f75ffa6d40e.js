75e0b9ba2624c4aaf9af41fc640e7349c24b8e0f
Daniel Mao <dm3wb@virginia.edu>
2019-03-03 10:13:24 -0500

    Updates

---
+++ precache-manifest.1dd0f31f80588b738dfe5f75ffa6d40e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63850d40530a880f3d35",
+    "url": "https//dm3wb.github.io/base6/static/js/runtime~main.63850d40.js"
+  },
+  {
+    "revision": "c14600fe97883eba08f5",
+    "url": "https//dm3wb.github.io/base6/static/js/main.c14600fe.chunk.js"
+  },
+  {
+    "revision": "02d2ce417cc01c62c84b",
+    "url": "https//dm3wb.github.io/base6/static/js/2.02d2ce41.chunk.js"
+  },
+  {
+    "revision": "c14600fe97883eba08f5",
+    "url": "https//dm3wb.github.io/base6/static/css/main.28d956e9.chunk.css"
+  },
+  {
+    "revision": "acabab8f8b19cb1c1e11acd4fcc96f89",
+    "url": "https//dm3wb.github.io/base6/index.html"
+  }
+];
